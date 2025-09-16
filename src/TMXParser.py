59700ec08c721c4ffcc0f1d6396c58457742803c
Derek Vawdrey <derek.vawdrey@gmail.com>
2025-09-15 18:48:24 -0600

    Adding requirements.txt

--- src/TMXParser.py
+++ src/TMXParser.py
@@ -1,3 +1,6 @@
+import lxml.etree as etree
+import PythonTmx as tmx
+
 class TMXParser:
 
     loadedPairs = []
@@ -6,7 +9,9 @@ class TMXParser:
         pass
 
     def load_tmx_file(self, file_path):
-        pass
-
-    def validate_tmx_file(self, file_path):
-        pass
+        tmx_file: etree._ElementTree = etree.parse(
+            file_path, etree.XMLParser(encoding="utf-8")
+        )
+        tmx_root: etree._Element = tmx_file.getroot()
+        tmx_obj: tmx.TmxElement = tmx.from_element(tmx_root)
+        assert isinstance(tmx_obj, tmx.Tmx), "The TMX file is not valid"
